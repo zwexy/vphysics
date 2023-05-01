114bd972868f050b8583a2557135c8dcf76a63ad
tyabus <tyabustest@gmail.com>
2023-05-01 17:53:11 +0600

    ivp_collision: Add another null checks

--- ivp_collision/ivp_mindist_recursive.cxx
+++ ivp_collision/ivp_mindist_recursive.cxx
@@ -78,7 +78,8 @@ void IVP_Mindist_Recursive::do_impact(){
 		
 		const IVP_Compact_Ledgetree_Node *n0 = l0->get_ledgetree_node();
 		const IVP_Compact_Ledgetree_Node *n1 = l1->get_ledgetree_node();
-		if ( n0->radius > n1->radius){
+		// tyabus: Crashes on de_mirage and other maps if n0 and n1 are not checked
+		if ((n0 && n1) && n0->radius > n1->radius) {
 		    recursive_status = IVP_MR_FIRST_SYNAPSE_RECURSIVE;
 		}else{
 		    recursive_status = IVP_MR_SECOND_SYNAPSE_RECURSIVE;
