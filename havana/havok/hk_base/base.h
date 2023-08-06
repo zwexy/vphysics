a0906091f926443e6fa96e179322efdf7b530e2a
tyabus <tyabustest@gmail.com>
2023-08-06 18:45:48 +0600

    havana: havok: Removed memory_managed_virtual_class,pre_allocated_array (unused)

--- havana/havok/hk_base/base.h
+++ havana/havok/hk_base/base.h
@@ -3,9 +3,7 @@
 
 #include <hk_base/base_types.h>
 #include <hk_base/memory/memory.h>
-#include <hk_base/memory/memory_managed_virtual_class.h>
 #include <hk_base/array/array.h>
-#include <hk_base/array/pre_allocated_array.h>
 #include <hk_base/display.h>
 
 extern void ivp_message(const char *templat, ...);
